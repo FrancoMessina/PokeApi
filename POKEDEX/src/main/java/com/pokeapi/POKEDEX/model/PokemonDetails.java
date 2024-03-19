de9ebf3589ee5469fcec5f2e94a9acb9cb7d2e1a
Franco Messina <franco.m2017@outlook.com>
2024-03-18 21:55:32 -0300

    Se agrego las foto al pokemon ya se ve bien. Solo falta el get all pokemons

--- POKEDEX/src/main/java/com/pokeapi/POKEDEX/model/PokemonDetails.java
+++ POKEDEX/src/main/java/com/pokeapi/POKEDEX/model/PokemonDetails.java
@@ -1,9 +1,12 @@
 package com.pokeapi.POKEDEX.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.util.List;
+import java.util.Map;
+
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 
@@ -15,4 +18,25 @@ public class PokemonDetails {
     private List<PokemonAbility> abilities;
     private List<PokemonMove> moves;
     private String description;
+
+    @JsonProperty("sprites")
+    private Map<String, Object> sprites;
+
+    public void setSprites(Map<String, Object> sprites) {
+        this.sprites = sprites;
+        if (sprites != null) {
+            Object other = sprites.get("other");
+            if (other instanceof Map) {
+                Map<String, Object> otherSprites = (Map<String, Object>) other;
+                Object dreamWorld = otherSprites.get("dream_world");
+                if (dreamWorld instanceof Map) {
+                    Map<String, Object> dreamWorldSprites = (Map<String, Object>) dreamWorld;
+                    Object frontDefault = dreamWorldSprites.get("front_default");
+                    if (frontDefault instanceof String) {
+                        this.photoUrl = (String) frontDefault;
+                    }
+                }
+            }
+        }
+    }
 }
