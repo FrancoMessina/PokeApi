ed06db80f30b2c01c9133aae2795440121531e40
Franco Messina <franco.m2017@outlook.com>
2024-03-19 09:52:33 -0300

    Swagger implementado

--- POKEDEX/src/main/java/com/pokeapi/POKEDEX/controller/PokemonController.java
+++ POKEDEX/src/main/java/com/pokeapi/POKEDEX/controller/PokemonController.java
@@ -4,8 +4,6 @@ import com.pokeapi.POKEDEX.model.Pokemon;
 import com.pokeapi.POKEDEX.model.PokemonDetails;
 import com.pokeapi.POKEDEX.service.PokemonService;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/pokemon")
+@RequestMapping("/pokemon")
 public class PokemonController {
     @Autowired
     private PokemonService pokemonService;
@@ -30,7 +28,7 @@ public class PokemonController {
     }
 
 
-    @GetMapping("")
+    @GetMapping("/all")
     public ResponseEntity<Page<PokemonDetails>> getPokemonPage(
             @RequestParam(defaultValue = "1") int page,
             @RequestParam(defaultValue = "5") int size) {
